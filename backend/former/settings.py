47555cc338dbcc624d7830e9ae575af9bb909a14
Matin Amani <mtin.amani@gmail.com>
2024-12-17 14:50:01 +0330

    backend: add .env.sample and use env for DB connection

--- backend/former/settings.py
+++ backend/former/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g+7t)v$6_#-6h!rr^v7zwyhprkzhh71v8%e!3#4#n&p+%1&4u*"
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -75,11 +78,11 @@ WSGI_APPLICATION = "former.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "former",
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
-        "PASSWORD": "npm_install",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+        "PASSWORD": env("DB_PASSWORD"),
     }
 }
 
